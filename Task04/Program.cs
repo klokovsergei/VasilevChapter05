77731447bb849b81e707d96fd97066a83feaf2d4
Sergei Klokov <s2584835@ya.ru>
2024-01-20 18:31:09 +0300

    feat: solve chapter05:task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу со статическим методом, аргументом которо­му передается символьный массив,
+// а результатом возвращается ссылка на целочисленный массив, состоящий из кодов символов из массива­ аргумента.
+
+static int[] ConvertFromCharToInt(char[] nums)
+{
+    int[] numsInt = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+        numsInt[i] = nums[i];
+    return numsInt;
+    
+}
+
+static char[] CreatAndFillCharArr(int size)
+{
+    char[] arr = new char[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = (char)random.Next('А','я');
+    return arr;
+}
+
+static void ShowArr(char[] nums)
+{
+    Console.Write($"[ {string.Join("\t", nums)} ]\n");
+}
+static void ShowArrInt(int[] nums)
+{
+    Console.Write($"[ {string.Join("\t", nums)} ]\n");
+}
+
+Console.WriteLine("Введите размерность символьного массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+char[] arrayChars = CreatAndFillCharArr(size);
+ShowArr(arrayChars);
+
+int[] arrayInt = ConvertFromCharToInt(arrayChars);
+ShowArrInt(arrayInt);
